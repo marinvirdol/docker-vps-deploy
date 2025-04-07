e453062e3acf2268823f4367c282aa69b0cb654f
Marin Virdol <marin@codemachine.dev>
2025-04-07 20:33:08 +0200

    debug

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,12 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 
+const dbUrl = process.env.DB_URL;
+
+if (!dbUrl) {
+  throw new Error("DB_URL is not set");
+}
+
 export const db = drizzle({
   connection: {
     connectionString: process.env.DB_URL,
