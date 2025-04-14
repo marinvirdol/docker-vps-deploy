10e6ee895c4eb6d5539bea82faddad7db6e36c46
Marin Virdol <marin@codemachine.dev>
2025-04-14 08:00:11 +0200

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+console.log("drizzle config", process.env.DB_URL);
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    // url: process.env.DB_URL!,
-    url: "postgresql://postgres:MyPassword123@91.99.3.185:5432/todos",
+    url: process.env.DB_URL!,
   },
 });
